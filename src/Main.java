5bfd94bd0a9552ebce8c095379e48586b50dddcf
Vinicius Alves <viniciusalvesms757@gmail.com>
2024-07-08 14:31:00 -0300

    feat: Curso, Conteudo, Mentoria.

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Mentoria mentoria = new Mentoria();
+
+
+
+    }
+}
